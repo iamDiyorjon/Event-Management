e59fd0cde6a576f53447bd47da48cb2baa98d24b
Diyor <shumtakadiyor@gmail.com>
2022-12-12 14:19:38 +0500

    Broker updated

--- ExamSolution/SqlExam/Brokers/Storages/StorageBroker.cs
+++ ExamSolution/SqlExam/Brokers/Storages/StorageBroker.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,23 @@ namespace SqlExam.Brokers.Storages
 {
      class   StorageBroker
     {
+        public readonly string connectionString= @"Server=(localdb)\mssqllocaldb; Database=MyDB";
         public async  Task<Room>InsertRoomAsync(Room room)
         {
+            using (var connection =
+    new SqlConnection(connectionString))
+            {
+                string query = $"insert into Room(\r\nname,capacity,price)values \r\n('{room.Name}'," +
+                    $"{room.Capacity}," +
+                    $"{room.Price})";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                await connection.OpenAsync();
+
+            await  command.ExecuteNonQueryAsync();
+
+                
+            }
             return room;
         }
          public  List<Room>SelectAllRooms()
@@ -22,11 +38,22 @@ namespace SqlExam.Brokers.Storages
         {
             throw new NotImplementedException();
         }
-        public async Task<Room>DeleteRoomAsync(int id)
+        public async Task<Room> DeleteRoomAsync(int id)
         {
-            throw new NotImplementedException();
-        }
+            Room room = new Room();
+            room.Id = id;
+            using (var connection =
+                 new SqlConnection(connectionString))
+            {
+                string query = $"";
+                SqlCommand command = new SqlCommand(query, connection);
 
+                await connection.OpenAsync();
+
+                await command.ExecuteNonQueryAsync();
+            }
+            return room;
+        }
         
         public async Task<Order> InsertOrderAsync(Order order)
         {
@@ -35,6 +62,7 @@ namespace SqlExam.Brokers.Storages
         public List<Order> SelectAllOrders()
         {
             throw new NotImplementedException();
+
         }
         public async Task<Order> UpdateOrderAsync(Order order)
         {
@@ -46,9 +74,28 @@ namespace SqlExam.Brokers.Storages
         }
         public async Task <User>InsertUserasync(User user)
         {
-            throw new NotImplementedException();
+            using (var connection =
+    new SqlConnection(connectionString))
+            {
+                string query = $"insert into Users(name,role,parol,login) values('{user.Name}', " +
+                    $"{user.Role}, " +
+                    $"'{user.Parol}', " +
+                    $"'{user.Login}')";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                await connection.OpenAsync();
+
+                await command.ExecuteNonQueryAsync();
+
+
+            }
+            return user;
         }
 
 
     }
 }
+
+
+
+ 
